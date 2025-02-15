786519a5b408db555e1693e3de73024293598e64
zubayer <zubayer.org@gmail.com>
2025-02-15 15:55:45 +0600

    hero name 40px

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-[45px] sm:text-[48px] font-thin -tracking-tight lg:mt-16 lg:text-6xl"
+              className="pb-16 text-[40px] sm:text-[48px] font-thin -tracking-tight lg:mt-16 lg:text-6xl"
             >
               Zubayer Ahamed
             </motion.h1>
